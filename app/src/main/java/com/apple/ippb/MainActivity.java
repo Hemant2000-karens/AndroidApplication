880a18c8b1e80613c370fbfdc74353e583be43aa
Hemant Kumar <team.karensbook@icloud.com>
2020-07-20 08:59:53 +0530

    Inventory Management Application version 1 for trial test

--- app/src/main/java/com/apple/ippb/MainActivity.java
+++ app/src/main/java/com/apple/ippb/MainActivity.java
@@ -2,12 +2,10 @@ package com.apple.ippb;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -84,9 +82,9 @@ public class MainActivity extends AppCompatActivity {
                 else
                 {
                     final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                    builder.setMessage("Invalid Uername or Password , Try Again");
-                    builder.setIcon(R.drawable.eroor);
                     builder.setCancelable(true);
+                    builder.setIcon(R.drawable.eroor);
+                    builder.setMessage("Invalid Uername or Password , Try Again");
                     builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialogInterface, int i) {
