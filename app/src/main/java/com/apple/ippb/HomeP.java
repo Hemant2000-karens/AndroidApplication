a69264a25102991c90376cf3c0c6bc696565ca46
hemant <team.karensbook@icloud.com>
2020-07-29 09:50:04 +0530

    Edited Many Thingd

--- app/src/main/java/com/apple/ippb/HomeP.java
+++ app/src/main/java/com/apple/ippb/HomeP.java
@@ -1,16 +1,23 @@
 package com.apple.ippb;
 
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Objects;
 
 public class HomeP extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_p);
-/*
 
         final TextView Nme_usr = findViewById(R.id.usr_name);
         String user = Objects.requireNonNull(getIntent().getExtras()).getString("name_vale");
@@ -25,31 +32,31 @@ public class HomeP extends AppCompatActivity {
                 final AlertDialog.Builder abc = new AlertDialog.Builder(HomeP.this);
                 abc.setMessage("You will be loged OUT !");//xloxk+"sec"*);
                 abc.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-               @Override
-               public void onClick(DialogInterface dialogInterface, int i) {
-                   try {
-                       LogOut();
-                   } catch (Exception e) {
-                       e.printStackTrace();
-                   }
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        try {
+                            LogOut();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
 
-               }
-           });
-           abc.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-               @Override
-               public void onClick(DialogInterface dialogInterface, int i) {
-                   dialogInterface.cancel();
-               }
-           });
+                    }
+                });
+                abc.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
 
-           AlertDialog amc = abc.create();
-           amc.show();
-           //JoptionPane Ends
+                AlertDialog amc = abc.create();
+                amc.show();
+                //JoptionPane Ends
 
-       }
-   });
+            }
+        });
 
-
+/*
         ImageButton plu = findViewById(R.id.add_buttin);
    plu.setOnClickListener(new View.OnClickListener() {
        @Override
@@ -119,13 +126,15 @@ public class HomeP extends AppCompatActivity {
     public void onBackPressed() {
 
     }
+*/
 
+        //External Save Instances
+    }
     private void LogOut() {
         Intent activity2Intent = new Intent(getApplicationContext(), MainActivity.class);
         finish();
         startActivity(activity2Intent);
 
- */
+
     }
-    //External Save Instances
 }
