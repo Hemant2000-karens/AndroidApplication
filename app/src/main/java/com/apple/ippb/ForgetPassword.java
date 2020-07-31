ed0477b87d3cdfe12f63fea1ddcb3d1882cef1b8
hemant <team.karensbook@icloud.com>
2020-07-31 12:39:36 +0530

    Edited Many Thingd

--- app/src/main/java/com/apple/ippb/ForgetPassword.java
+++ app/src/main/java/com/apple/ippb/ForgetPassword.java
@@ -5,30 +5,51 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class ForgetPassword extends AppCompatActivity {
-    public static String emailid;
 
-    public static String Email() {
-        return emailid;
-    }
-
+    EditText emailh = findViewById(R.id.emailtxt);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.forget_activity);
 
-        final EditText emailh = findViewById(R.id.emailtxt);
-
         Button SendButton = findViewById(R.id.sendbutton);
         SendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                emailid = emailh.getText().toString();
-                Intent i = new Intent(getApplicationContext(), verifyotp.class);
+                FirebaseAuth auth = FirebaseAuth.getInstance();
+                auth.setLanguageCode("en");
+                auth.sendPasswordResetEmail(emailh.getText().toString().trim())
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(getApplicationContext(), "Email Reset Link Send Successfully", Toast.LENGTH_LONG).show();
+                                } else
+                                    Toast.makeText(getApplicationContext(), "Sorry! , Try Again", Toast.LENGTH_LONG).show();
+                            }
+                        });
+            }
+        });
+
+
+        ImageButton bc = findViewById(R.id.login_back);
+        bc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(i);
+                finish();
             }
         });
 
